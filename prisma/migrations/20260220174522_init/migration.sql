08850a92e633d5a16993e23984f74cf4f11989fb
jirayu <jirayu.ct.dev@gmail.com>
2026-02-21 00:59:31 +0700

    init prisma

---
+++ prisma/migrations/20260220174522_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('VEGETABLE', 'FRUIT', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "unit" TEXT NOT NULL,
+    "category" "Category" NOT NULL,
+    "inStock" BOOLEAN NOT NULL DEFAULT true,
+    "stockQty" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
